f32d5c6d14c036da2afdc599345f0b9e34389bc5
govind6498 <govindsingh6498@gmail.com>
2021-09-28 19:13:24 +0530

    REFECTOR:To add multiple address Book

--- src/com/bridgelabz/addressbookprogram/AddressBookIF.java
+++ src/com/bridgelabz/addressbookprogram/AddressBookIF.java
@@ -1,10 +1,17 @@
 package com.bridgelabz.addressbookprogram;
 
 public interface AddressBookIF {
-	public void addContact(ContactPerson person);
-	public void operation();
-	public void addContact();
-	public void displayContect();
-	public void editPerson();
-	public void deletePerson();
+//	public void addContact(ContactPerson person);
+//	public void operation();
+//	public void addContact();
+//	public void displayContect();
+//	public void editPerson();
+//	public void deletePerson();
+	public void addAddressBook();
+	public void operationDirectory();
+	public void displayDirectoryContents();
+	public void editAddressBook();
+	void displayContents();
+	void addContact();
+	void operation();
 }
